86e70e81dd78ea13bf2b247a695b9794c1a48863
matt_kuras <mattkuras@gmail.com>
2021-04-28 15:01:34 -0400

    front end successfully hit endpoint sending email prompting user to successfully reset password.

--- app/javascript/src/components/registrations/login.js
+++ app/javascript/src/components/registrations/login.js
@@ -1,11 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import axios from 'axios'
 
 const Login = (props) => {
 
     const [email, setEmail] = useState("");
+    const [email2, setEmail2] = useState("");
     const [password, setPassword] = useState("");
+    const [forgotPassword, setForgotPassword] = useState(false)
+    const [messageSent, setMessageSent] = useState(false)
+    const [message, setMessage] = useState('')
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -33,35 +37,72 @@ const Login = (props) => {
     const redirect = () => {
         history.push("/dashboard");
     };
+
+    const resetPassword = (event) => {
+        event.preventDefault()
+        console.log('clicked')
+        axios.post("/password/forgot", { email_address: email2 })
+        .then(resp => {
+            if(resp.data.status == 'ok'){
+                setMessage(resp.data.message)
+            }
+        })
+        
+    }
+
+    const ForgotPasswordForm = () => (
+            <div className="input-contain">
+                <label for="username">Enter you email </label>
+                <input
+                    className="user-input"
+                    type="text"
+                    placeholder="Enter Email"
+                    value={email2}
+                    onChange={(e) => setEmail2(e.target.value)}
+                />
+            <input className='button' type="submit" onClick={resetPassword}/>
+            </div>
+    )
+
+    const renderOk = () => {
+        return (
+            <div className='message-sent'>
+                <p>{message}</p>
+            </div>
+        )
+    }
     return (
         <div>
             <div className='login-container'>
-            <form className="login-form" onSubmit={handleSubmit}>
-        <div className="input-contain">
-          <label for="username">Email: </label>
-          <input
-            className="user-input"
-            type="text"
-            placeholder="Enter Username"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="input-contain">
-          <label for="password">Password: </label>
-          <input
-            className="user-input"
-            type="password"
-            placeholder="Enter Password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <input className='button' type="submit" />
-      </form>
+                <form className="login-form" onSubmit={handleSubmit}>
+                    <div className="input-contain">
+                        <label for="username">Email: </label>
+                        <input
+                            className="user-input"
+                            type="text"
+                            placeholder="Enter Email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="input-contain">
+                        <label for="password">Password: </label>
+                        <input
+                            className="user-input"
+                            type="password"
+                            placeholder="Enter Password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <input className='button' type="submit" />
+                </form>
+                {forgotPassword ? <ForgotPasswordForm/> : <a onClick={e => setForgotPassword(true)}><p>forgot password?</p></a>}
+                {/* <ForgotPasswordForm/> */}
             </div>
+            {messageSent ? renderOk() : null}
         </div>
     );
 }
